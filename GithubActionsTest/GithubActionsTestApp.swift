803e34a93c46e5713ea268bf5563366b159e52fd
Katopan <76249494+katomizuki@users.noreply.github.com>
2022-10-01 16:12:32 +0900

    Initial Commit

---
+++ GithubActionsTest/GithubActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsTestApp.swift
+//  GithubActionsTest
+//
+//  Created by ミズキ on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
